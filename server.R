b82c9435b6b6df9c5b87b7bef6fb2227d826a09c
Mark IIH <mark@iihnordic.com>
2016-06-28 14:21:55 +0200

    working google analytics login

--- server.R
+++ server.R
@@ -1,6 +1,21 @@
+library(shiny)
+library(googleAuthR)
+library(googleAnalyticsR)
+options(shiny.port = 1221)
 
 function(input, output, session){
   
+  access_token <- callModule(googleAuthR::googleAuth, "auth")
   
+  ga_tables <- reactive({
+    validate(
+      need(access_token(), "Authenticate")
+    )
+    with_shiny(google_analytics_account_list,
+               shiny_access_token = access_token())
+    
+  })
+  
+  selected_id <- callModule(authDropdown, "auth_dropdown", ga_tables)
   
 }
--- ui.R
+++ ui.R
@@ -1,6 +1,9 @@
 library(shiny)
+library(googleAnalyticsR)
 
 htmlTemplate("index.html",
+             googleLogin = googleAuthR::googleAuthUI("auth"),
+             authDropdown = googleAnalyticsR::authDropdownUI("auth_dropdown"),
              button = actionButton("action", "Action"),
              slider = sliderInput("x", "X", 1, 100, 50)
 )
